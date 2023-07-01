13e1c8fac96ffc3ea5bdfcab7109ee3a64d80adc
deeikhteamofc <119467977+deeikhteamofc@users.noreply.github.com>
2023-07-01 09:24:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94740804536', 'Dinuwa Official²⁰²³', true],
-  ['94740804536', 'Dinuwa Official²⁰²³', true], 
+  ['94763422146', 'Sʜᴇʟʙʏㅤxㅤɴᴢᴇ', ', true],
+  ['94771540427', 'Dinuwa Official²', true], 
   [''] 
 ] //Number of owners
 
